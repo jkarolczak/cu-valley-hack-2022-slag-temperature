6814a509781e71b10daf4672b34a24e83589480e
AleksandraSwierkowska <aleksandraswierkowska99@gmail.com>
2022-03-11 18:39:16 +0100

    add app file

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+# import warnings
+# warnings.filterwarnings('ignore') # https://github.com/streamlit/streamlit/issues/1430
+
+# csv_file = st.file_uploader(label="Wprowadź dane")
+# if csv_file:
+#     if not csv_file.name.endswith(".csv"):
+#         st.error("Proszę wprowadzić plik z rozszerzeniem csv")
+#     else:
+#         df = pd.read_csv(csv_file)
+
