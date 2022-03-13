a51fbf6f1ec49b0add3ea8abe57941c2535b72a2
jkarolczak <jacek.karolczak@outlook.com>
2022-03-13 03:37:01 +0100

    Enhance the project documentation in README.md and fix environment files issues

--- README.md
+++ README.md
@@ -3,17 +3,57 @@
 ## Inferencja
 
 1. Przygotowanie repozytorium:
-    1. Sklonuj to repozytorium <br> ```git clone https://github.com/jkarolczak/cu-valley-hack-2022-slag-temperature.git```
+    1. Sklonuj to
+       repozytorium <br> ```git clone https://github.com/jkarolczak/cu-valley-hack-2022-slag-temperature.git```
     2. Przejdź do sklonowanego repozytorium <br> ```cd cu-valley-hack-2022-slag-temperature```
 2. Przygotuj środowisko wykonawcze:
-    1. Utwórz środowisko przy użyciu menedźera Conda uruchamiając polecenie <br>```conda env create -f environment.yaml```
+    1. Utwórz środowisko przy użyciu menedźera Conda uruchamiając
+       polecenie <br>```conda env create -f environment.yaml```
     2. Aktywuj utworzone środowisko wykonując polecenie <br>```conda activate cu-valley-slag```
 3. Przygotowanie danych:
     1. Skopiuj archiwum zawierające dane do przeprowadzenia inferencji do
        repozytorium <br> ```mv <path-to-archive.zip> data.zip```
 4. Przeprowadzenie inferencji:
-    1. Uruchom polecenie <br> ```python scripts/inference_pipeline.py --archive <archive-name.zip>``` <br> Jeśli archiwum z danymi
-       nazywa się `data.zip`, argument --archive może zostać pominięty, gdyż `data.zip` jest jego domyślną wartością. W
-       takim przypadku wystarczy uruchomić polecenie <br> ```python scripts/inference_pipeline.py``` <br>
+    1. Uruchom polecenie <br> ```python scripts/inference_pipeline.py --archive <archive-name.zip>``` <br> Jeśli
+       archiwum z danymi nazywa się `data.zip`, argument --archive może zostać pominięty, gdyż `data.zip` jest jego
+       domyślną wartością. W takim przypadku wystarczy uruchomić
+       polecenie <br> ```python scripts/inference_pipeline.py``` <br>
     2. Wyniki inferencji znajdują się w pliku `predictions.csv`, który znajduje się w katalogu, w którym obecnie się
        znajdujesz.
+
+## Panel kontrolny
+
+Aby uruchomić panel kontrolny należy wykonać następujące polecenie:
+
+```
+sreamlit run app.py
+```
+
+Działające demo znajduje się na stronie [miedziaki.tech](http://miedziaki.tech)
+Przygotowany mockup imituje panel kontrolny operatora pieca zawiesinowego w KGHM Hucie Miedzi “Głogów II”. Panel ten
+pokazuje jak w prosty sposób nasze rozwiązanie może zostać zintegrowane z już istniejącymi oraz wdrożonymi systemami.
+Dzięki takiej integracji operatorzy powinni łatwo zaadaptować nowe informacje. Prezentacja czynników wpływających na
+predykcję zwraca uwagę operatora na istotne zmiany parametrów procesowych. Co za tym idzie ryzyko błędu ludzkiego jest
+znacząco redukowane.
+
+## Trenowanie modelu oraz eksperymentowanie
+
+W celu trenowania modelu oraz eksperymentowania (porównywania modeli, optymalizacji hiperparametrów itp.), w
+katalogu `scripts` zostały przygotowane następujące skypty:
+
+- `parse_data.py` - skrypt parsujący dane wejściowe, agregujący je dla odpowieniego okna czasu przy użyciu różnych
+  statystyk oraz zapisujący przygotowane dane do pliku
+- `variable_selection.py` - skrypt ograniczający liczbę zmiennych w pliku do tych uprzednio zdefiniowanych
+- `data_split.py` - skrypt dzielący zbiór danych na zbiory testowy, treningowy oraz holdout
+- `train_model.py` - skrypt tworzący oraz trenujący nowy model
+- `infer_model.py` - skrypt służący ewaluacji modelu
+- `inference_pipeline.py` - skrypt służący przygotowaniu predykcji w sposób kompleksowy, przygotowany na potrzeby
+  ewaluacji modelu przez organizatorów hackathonu. Więcej infomracji dostępncyh jest w sekcji powyżej (Inferencja).
+
+Wszystkie skrypty są konfigurowane plikami konfiguracyjnymi, które znajdują się w katalogu `resources/cfg`. Takie
+rozwiazanie umożliwia orkiestrację całego procesu eksperymentowania oraz ułatwia proces tworzenia modelu przez
+użytkownika.
+
+## Analiza danych
+
+Proces analizy oraz jego rezultaty znajdują się pliku `analysis.ipynb`
--- app.py
+++ app.py
@@ -1,50 +1,25 @@
-import streamlit as st
+import pickle
+import time
+from datetime import datetime
+
 import pandas as pd
 import numpy as np
-import pickle
-import shap
-import time
-
-import streamlit.components.v1 as components
 import plotly.express as px
-
-from datetime import datetime
+import shap
+import streamlit as st
+import streamlit.components.v1 as components
 from PIL import Image
 
-if 'time' not in st.session_state:  # monitor time change
-    st.session_state['time'] = 0
 
-favicon = Image.open('static/coin.png')
-st.set_page_config(
-    page_title="Miedź na oku",
-    page_icon=favicon,
-    layout="wide",
-    # initial_sidebar_state="expanded"
-)
-
-test_data = pd.read_csv("static/mockup_data.csv")
-
-model = pickle.load(open("static/model.pkl", 'rb'))
-explainer = shap.Explainer(model)
-
-for i, row in test_data[:30].iterrows():  # calculate temp for the first 30 minutes
-    model_input = pd.DataFrame(row.drop(["czas", "temp"]), dtype=np.float64).transpose()
-    temp = np.round(model.predict(model_input)[0])
-    test_data.at[i, "temp"] = temp
-
-
-def draw_plot(plot, time_window, step):
+def draw_plot(plot: st.delta_generator.DeltaGenerator, time_window: int, step: int, test_data: pd.DataFrame) -> None:
     fig = px.line(test_data.iloc[max(step - time_window, 0):step + 1], x="czas", y="temp", markers=True,
-                  labels={
-                      "czas": "Czas",
-                      "temp": "Temperatura"
-                  })
+                  labels={"czas": "Czas", "temp": "Temperatura"})
     fig.add_hline(y=1310, line_dash="dash", line_color="red")
     fig.update_layout(yaxis_range=[1290, 1315])
     plot.plotly_chart(fig, use_container_width=True)
 
 
-def draw_shap(shap_values):
+def draw_shap(shap_values: shap._explanation.Explanation) -> None:
     shap_plot = shap.plots.force(shap_values[0])
     shap_html = f"<head>{shap.getjs()}</head><body>{shap_plot.html()}</body>"
     components.html(shap_html)
@@ -65,47 +40,65 @@ def parse_columns(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-st.title("Panel kontrolny")
+def main() -> None:
+    if 'time' not in st.session_state:
+        st.session_state['time'] = 0
 
-container = st.empty()
+    favicon = Image.open('static/coin.png')
+    st.set_page_config(page_title="Miedź na oku", page_icon=favicon, layout="wide")
 
-time_window = st.slider("Przedział pobierania pomiarów w minutach", min_value=3, max_value=60, value=30)
+    test_data = pd.read_csv("static/mockup_data.csv")
+    model = pickle.load(open("static/model.pkl", 'rb'))
+    explainer = shap.Explainer(model)
 
-st.write('Temperatura żużlu wewnątrz pieca zawiesinowego Huty Miedzi KGHM "Głogów II"')
-
-plot = st.empty()
-
-st.write('Wpływ parametrów na temperaturę')
-container_shap = st.empty()
-
-for i, row in test_data[30 + (st.session_state.time) % 29:].iterrows():
-    st.session_state.time += 1
-    with container.empty():
-        col1, col2, col3, col4 = container.columns(4)
-        timestamp = datetime.strptime(row['czas'], '%Y-%m-%d %H:%M:%S')
+    for i, row in test_data[:60].iterrows():
         model_input = pd.DataFrame(row.drop(["czas", "temp"]), dtype=np.float64).transpose()
-
-        model_input = parse_columns(model_input)
-
         temp = np.round(model.predict(model_input)[0])
-        test_data.at[i, "temp"] = temp
+        test_data.at[i, "temp"] = int(temp)
 
-        col1.metric("Data", timestamp.strftime('%d/%m/%y'))
-        col2.metric("Czas", timestamp.strftime('%H:%M'))
+    st.title("Panel kontrolny")
+    container = st.empty()
+    st.subheader('Temperatura żużlu wewnątrz pieca zawiesinowego Huty Miedzi KGHM "Głogów II"')
+    cols = st.columns(3)
+    with cols[0]:
+        time_window = st.slider("Liczba wyświetlanych minionych minut", min_value=3, max_value=60, value=30)
 
-        if "last_temp" in locals():
-            col3.metric("Temperatura", f'{temp} °C', f'{temp - last_temp} °C', delta_color='off')
-        else:
-            col3.metric("Temperatura", f'{temp} °C')
-        if int(temp) > 1310:
-            col4.metric("", "⚠️")
-        else:
-            col4.metric("", "✅")
-        last_temp = temp
+    plot = st.empty()
+    st.subheader('Wpływ parametrów na temperaturę')
+    container_shap = st.empty()
 
-        draw_plot(plot, time_window, i)
+    for i, row in test_data[30 + st.session_state.time % 29:].iterrows():
+        st.session_state.time += 1
+        with container.empty():
+            col1, col2, col3, col4 = container.columns(4)
+            timestamp = datetime.strptime(row['czas'], '%Y-%m-%d %H:%M:%S')
+            model_input = pd.DataFrame(row.drop(["czas", "temp"]), dtype=np.float64).transpose()
 
-    with container_shap.empty():
-        shap_values = explainer(model_input)
-        draw_shap(shap_values)
-        time.sleep(5)
+            model_input = parse_columns(model_input)
+
+            temp = np.round(model.predict(model_input)[0])
+            test_data.at[i, "temp"] = int(temp)
+
+            col1.metric("Data", timestamp.strftime('%d/%m/%y'))
+            col2.metric("Czas", timestamp.strftime('%H:%M'))
+
+            if "last_temp" in locals():
+                col3.metric("Temperatura", f'{temp} °C', f'{temp - last_temp} °C', delta_color='off')
+            else:
+                col3.metric("Temperatura", f'{temp} °C')
+            if int(temp) > 1310:
+                col4.metric("", "⚠️")
+            else:
+                col4.metric("", "✅")
+            last_temp = temp
+
+            draw_plot(plot, time_window, i, test_data)
+
+        with container_shap.empty():
+            shap_values = explainer(model_input)
+            draw_shap(shap_values)
+            time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
